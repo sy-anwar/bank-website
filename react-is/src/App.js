62cca087cc8e193f0f051e02c48f09e3a1e94968
Irena Irmalasari <13517100@std.stei.itb.ac.id>
2019-11-18 22:41:28 +0700

    add Homepage, Header, Login not styled yet

---
+++ react-is/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import TransactionHistory from './TransactionHistory.js';
+import Homepage from './Homepage.js';
+import './App.css';
+
+function App() {
+  return (
+    <Homepage />
+  )
+}
+
+export default App;
